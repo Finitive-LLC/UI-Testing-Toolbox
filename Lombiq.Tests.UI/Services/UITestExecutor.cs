13dfb41be86d0af43d54a9a105f3fccb6993d7a9
Krisztián Németh <krisztian.nemeth@lombiq.com>
2022-04-03 11:13:09 +0200

    Searching for max value

--- Lombiq.Tests.UI/Services/UITestExecutor.cs
+++ Lombiq.Tests.UI/Services/UITestExecutor.cs
@@ -12,7 +12,7 @@ namespace Lombiq.Tests.UI.Services
 {
     public static class UITestExecutor
     {
-        private static readonly SemaphoreSlim _semaphoreSlim = new(12);
+        private static readonly SemaphoreSlim _semaphoreSlim = new(14);
 
         /// <summary>
         /// Executes a test on a new Orchard Core web app instance within a newly created Atata scope.
