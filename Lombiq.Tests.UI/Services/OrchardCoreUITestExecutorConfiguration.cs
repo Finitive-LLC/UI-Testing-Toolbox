bf1cd471873ba1f7156753e737240739a3aec199
Krisztián Németh <krisztian.nemeth@lombiq.com>
2022-04-04 23:30:36 +0200

    Use ProcessorCount by default

--- Lombiq.Tests.UI/Services/OrchardCoreUITestExecutorConfiguration.cs
+++ Lombiq.Tests.UI/Services/OrchardCoreUITestExecutorConfiguration.cs
@@ -26,8 +26,11 @@ namespace Lombiq.Tests.UI.Services
 
         public int MaxRetryCount { get; set; } =
             TestConfigurationManager.GetIntConfiguration("OrchardCoreUITestExecutorConfiguration:MaxRetryCount", 2);
+
         public int MaxRunningConcurrentTests { get; set; } =
-            TestConfigurationManager.GetIntConfiguration("OrchardCoreUITestExecutorConfiguration:MaxRunningConcurrentTests", 0);
+            TestConfigurationManager.GetIntConfiguration(
+                "OrchardCoreUITestExecutorConfiguration:MaxRunningConcurrentTests",
+                Environment.ProcessorCount);
 
         public TimeSpan RetryInterval { get; set; } =
             TimeSpan.FromSeconds(TestConfigurationManager.GetIntConfiguration("OrchardCoreUITestExecutorConfiguration:RetryIntervalSeconds", 0));
